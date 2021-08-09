25d2eaa7f61ce39c823893953ef1ba84cb663e4a
tmdeh <66948129+tmdeh@users.noreply.github.com>
2021-08-10 00:51:14 +0900

    aaa

--- src/test/java/com/jojoldu/book/springboot/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/dto/HelloResponseDtoTest.java
@@ -14,7 +14,8 @@ public class HelloResponseDtoTest {
         int amount = 1000;
 
         //when
-        HelloResponseDto dto = new HelloResponseDto(name, amount);
+        HelloResponseDto dto = new HelloResponseDto(name,
+                amount);
 
         //then
         assertThat(dto.getName()).isEqualTo(name);
